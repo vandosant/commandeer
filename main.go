be6154cc800df21b4aede7fcb59f618225365121
vandosant <sjskender@gmail.com>
2015-07-26 08:39:19 -0600

    Create mongo db session

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
 
 	"github.com/vandosant/commandeer/models"
+	"gopkg.in/mgo.v2"
 )
 
 var commands models.Commands
+
+var (
+	mongoURL        = os.Getenv("mongo_url")
+	mongoUser       = os.Getenv("mongo_user")
+	mongoPassword   = os.Getenv("mongo_password")
+	mongoCollection = os.Getenv("mongo_collection")
+	mongoSession    *mgo.Session
+	database        *mgo.Database
+	collection      *mgo.Collection
+	err             error
+)
 
 func CommandHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/vnd.application+json; charset=UTF-8")
@@ -34,6 +47,16 @@ func CommandHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	addr := fmt.Sprintf("mongodb://%s:%s@%s", mongoUser, mongoPassword, mongoURL)
+	if mongoSession, err = mgo.Dial(addr); err != nil {
+		log.Fatal(err)
+	}
+	defer mongoSession.Close()
+
+	database = mongoSession.DB("")
+	collection = database.C(mongoCollection)
+	fmt.Printf("Collection: %+v", collection)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
